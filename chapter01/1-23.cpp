2ce24f56273a33179db7c2cab9db2d5e06194603
Luka M <Luka M>
2017-02-15 14:54:01 +0800

    exercise1-23

---
+++ chapter01/1-23.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+	Sales_item currItem, item;
+	if (std::cin >> currItem) {
+		int cnt = 1;
+		while (std::cin >> item) {
+			if (item.isbn() == currItem.isbn())
+				++cnt;
+			else {
+				std::cout << currItem << " occurs " 
+					<< cnt << " times " << std::endl;
+				currItem = item;
+				cnt = 1;
+			}
+		}
+		std::cout << currItem << " occurs "
+			<< cnt << " times " << std::endl;
+	}
+	return 0;
+}
