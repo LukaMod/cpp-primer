8cf342a3a399a7326d72e8693aaa3a8947637338
Luka M <Luka M>
2017-02-17 16:09:07 +0800

    exercise3-02

---
+++ chapter03/3-02-a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cin;
+using std::getline;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	string line;
+	getline(cin, line);
+	cout << line << endl;
+	
+	return 0;
+}
---
+++ chapter03/3-02-b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	string word;
+	cin >> word;
+	cout << word << endl;
+	
+	return 0;
+}
