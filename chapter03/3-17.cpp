89897e3bbc19c403b5427ed7717f30d0b4a70941
Luka <1004529488@qq.com>
2017-02-18 00:57:56 +0800

    exercise3-17

---
+++ chapter03/3-17.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using std::vector;
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	vector<string> vec;
+	string str;
+	while (cin >> str)
+		vec.push_back(str);
+	for (auto &s : vec) {
+		for (auto &c : s)
+			c = toupper(c);
+	}
+	for (auto s : vec)
+		cout << s << endl;
+	return 0;
+}
