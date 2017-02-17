bc3535f5d82d99de2fa3f784e58512a08cd68d3e
Luka <1004529488@qq.com>
2017-02-17 23:01:18 +0800

    exercise3-10

---
+++ chapter03/3-10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	string str;
+	cout << "Enter a string including punctuations:" << endl;
+	getline(cin, str);
+	for (auto c : str) {
+		if (!ispunct(c))
+			cout << c;
+	}
+	cout << endl;
+	return 0;
+}
