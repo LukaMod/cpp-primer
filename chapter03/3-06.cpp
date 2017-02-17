450f974c677db4e00e5930075322c3765d386d6d
Luka M <Luka M>
2017-02-17 17:28:14 +0800

    exercise3-06

---
+++ chapter03/3-06.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	string str("An sample string");
+	for (auto &c : str)
+		c = 'X';
+	cout << str << endl;
+	return 0;
+}
