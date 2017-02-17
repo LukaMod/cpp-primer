c6e2d49e2226c3cfb710b7027b2b2e383180d452
Luka M <Luka M>
2017-02-17 17:28:22 +0800

    exercise3-08

---
+++ chapter03/3-08.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	string str("An sample string");
+	decltype(str.size()) i = 0;
+
+	while (i != str.size()) {
+		str[i] = 'X';
+		++i;
+	}
+	cout << str << endl;
+
+	for (i = 0; i != str.size(); ++i)
+		str[i] = 'Y';
+	cout << str << endl;
+
+	return 0;
+}
