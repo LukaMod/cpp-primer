7bd56594386d153506fe253649bd2110c8561d08
Luka M <Luka M>
2017-02-17 16:24:21 +0800

    exercise3-04

--- chapter03/3-04-b.cpp
+++ chapter03/3-04-b.cpp
@@ -11,7 +11,7 @@ int main()
 	string str1, str2;
 	cin >> str1 >> str2;
 	if (str1.size() == str2.size())
-		cout << "Equal: " << str1 << endl;
+		cout << "Equal length: " << str1 << endl;
 	else
 		cout << "The longer: " << (str1.size() > str2.size() ? str1 : str2) << endl;
 	
