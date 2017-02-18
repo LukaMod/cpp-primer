761c5175c7da8d704ff8b755ea532d72a149e45c
Luka <1004529488@qq.com>
2017-02-18 17:10:07 +0800

    exercise3-25

---
+++ chapter03/3-25.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	vector<unsigned> scores(11, 0);
+	unsigned grade;
+	while (cin >> grade) {
+		++*(scores.begin() + grade / 10);
+	}
+	for (auto i : scores)
+		cout << i << " ";
+	cout << endl;
+
+	return 0;
+}
