27bc4b5f4016a90f00cb58cb6480b53eeb28f54f
Luka <1004529488@qq.com>
2017-02-18 15:41:54 +0800

    exercise3-22

---
+++ chapter03/3-22.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using std::vector;
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	vector<string> text;
+	string line;
+	while (getline(cin, line))
+		text.push_back(line);
+	for (auto it = text.begin(); it != text.end() && !it->empty(); ++it) {
+		for (auto &c : *it)
+			c = toupper(c);
+		cout << *it;
+	}
+
+	return 0;
+}
