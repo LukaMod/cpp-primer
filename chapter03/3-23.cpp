956e9dd70eeb09daa12161723f240870d193fa6b
Luka <1004529488@qq.com>
2017-02-18 15:45:37 +0800

    exercise2-23

---
+++ chapter03/3-23.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	vector<int> vec(10, 7);
+	for (auto it = vec.begin(); it != vec.end(); ++it)
+		*it *= 2;
+	for (int val : vec)
+		cout << val << " ";
+	cout << endl;
+
+	return 0;
+}
