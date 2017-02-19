1a74d0b1e61fc5f1d0475a47956713260669f600
Luka <1004529488@qq.com>
2017-02-19 23:09:17 +0800

    exercise3-35

---
+++ chapter03/3-35.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iterator>
+
+using std::cout;
+using std::endl;
+using std::begin;
+using std::end;
+
+int main()
+{
+	int ia[10];
+	int *beg = begin(ia);
+	int *last = end(ia);
+	for (; beg != last; ++beg)
+		*beg = 0;
+	for (auto i : ia)
+		cout << i << " ";
+	cout << endl;
+	return 0;
+}
