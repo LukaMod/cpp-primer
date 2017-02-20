c9fc070267539fd0709a78deff8267443d25e363
Luka M <Luka M>
2017-02-20 16:22:32 +0800

    exercise3-40

---
+++ chapter03/3-40.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	const char ca1[6] = "hello";
+	const char ca2[6] = "world";
+	const size_t size = strlen(ca1) + strlen(" ") + strlen(ca2) + 1;
+	char cstr[size];
+	strcpy(cstr, ca1);
+	strcat(cstr, " ");
+	strcat(cstr, ca2);
+	cout << cstr << endl;
+	return 0;
+}
