c158558c097ef5f2fdd595e2124803371b9f9fce
Luka M <Luka M>
2017-02-20 16:54:00 +0800

    update

--- chapter01/1-16.cpp
+++ chapter01/1-16.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 int main()
 {
-	int sum = 0, val = 0;
+    int sum = 0, val = 0;
 
-	while (std::cin >> val)
-		sum += val;
-	std::cout << "Sum is: " << sum << std::endl;
+    while (std::cin >> val)
+	sum += val;
+    std::cout << "Sum is: " << sum << std::endl;
 
-	return 0;
+    return 0;
 }
