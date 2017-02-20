e3ace90f4460d3a8aeac257cadc1ec0cbdf19855
Luka <1004529488@qq.com>
2017-02-20 20:49:33 +0800

    update

--- chapter01/1-11.cpp
+++ chapter01/1-11.cpp
@@ -2,20 +2,22 @@
 
 int main()
 {
-	int v1 = 0, v2 = 0;
-	std::cout << "Enter two numbers:" << std::endl;
-	std::cin >> v1 >> v2;
-	if (v1 > v2) {
+    int v1 = 0, v2 = 0;
+    std::cout << "Enter two numbers:" << std::endl;
+    std::cin >> v1 >> v2;
+    if (v1 > v2)
+    {
 		int temp;
 		temp = v1;
 		v1 = v2;
 		v2 = temp;
-	}
-	std::cout << "Numbers between " << v1 << " and " << v2 << " are :" << std::endl;
-	while (v1 <= v2) {
+    }
+    std::cout << "Numbers between " << v1 << " and " << v2 << " are :" << std::endl;
+    while (v1 <= v2)
+    {
 		std::cout << v1 << " ";
 		++v1;
-	}
-	std::cout << std::endl;
-	return 0;
+    }
+    std::cout << std::endl;
+    return 0;
 }
