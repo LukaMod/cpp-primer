e0d1e4b6d951a3282c3f1b22ac4e739c32b07b60
Luka <1004529488@qq.com>
2017-02-20 21:03:32 +0800

    update

--- chapter02/2-41-20.cpp
+++ chapter02/2-41-20.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <string>
 
-struct Sales_data {
-	std::string bookNo;
-	unsigned units_sold = 0;
-	double revenue = 0.0;
+struct Sales_data
+{
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
 };
 
 int main()
 {
-	Sales_data data;
-	double price;
-	while (std::cin >> data.bookNo >> data.units_sold >> price) {
-		data.revenue = data.units_sold * price;
-		std::cout << data.bookNo << " " << data.units_sold << " "
-			<< data.revenue << " " << price << std::endl;
-	}
-	return 0;
+    Sales_data data;
+    double price;
+    while (std::cin >> data.bookNo >> data.units_sold >> price)
+    {
+	data.revenue = data.units_sold * price;
+	std::cout << data.bookNo << " " << data.units_sold << " "
+		  << data.revenue << " " << price << std::endl;
+    }
+    return 0;
 }
--- chapter02/2-42-20.cpp
+++ chapter02/2-42-20.cpp
@@ -3,12 +3,13 @@
 
 int main()
 {
-	Sales_data data;
-	double price;
-	while (std::cin >> data.bookNo >> data.units_sold >> price) {
-		data.revenue = data.units_sold * price;
-		std::cout << data.bookNo << " " << data.units_sold << " "
-			<< data.revenue << " " << price << std::endl;
-	}
-	return 0;
+    Sales_data data;
+    double price;
+    while (std::cin >> data.bookNo >> data.units_sold >> price)
+    {
+	data.revenue = data.units_sold * price;
+	std::cout << data.bookNo << " " << data.units_sold << " "
+		  << data.revenue << " " << price << std::endl;
+    }
+    return 0;
 }
