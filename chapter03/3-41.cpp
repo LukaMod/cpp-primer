46d8e77fbbd040fbf0180609fc7337dcc2aeb4e8
Luka M <Luka M>
2017-02-21 10:23:33 +0800

    exercise3-41

--- chapter03/3-41.cpp
+++ chapter03/3-41.cpp
@@ -8,10 +8,10 @@ using std::end;
 
 int main()
 {
-	int a[] = { 1, 2, 3, 4, 5, 6 };
-	vector<int> vec(begin(a), end(a));
-	for (int i : vec)
-		cout << i << " ";
-	cout << endl;
-	return 0;
+    int a[] = {1, 2, 3, 4, 5, 6};
+    vector<int> vec(begin(a), end(a));
+    for (int i : vec)
+        cout << i << " ";
+    cout << endl;
+    return 0;
 }
