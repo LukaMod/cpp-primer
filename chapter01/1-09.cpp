6d5c1f00ae4e8b2a2a367f49f5dc6396c3c79fb9
Luka M <Luka M>
2017-02-21 09:38:41 +0800

    update

--- chapter01/1-09.cpp
+++ chapter01/1-09.cpp
@@ -1,22 +1,11 @@
 #include <iostream>
-int main()
-{
-    int sum = 0, val = 50;
+int main() {
+  int sum = 0, val = 50;
 
-    while (val <= 100)
-    {
-<<<<<<< HEAD
-        sum += val;
-        ++val;
-    }
-    std::cout << "Sum of 50 to 100 inclusive is "
-              << sum << std::endl;
-=======
-	sum += val;
-	++val;
-    }
-    std::cout << "Sum of 50 to 100 inclusive is "
-	      << sum << std::endl;
->>>>>>> d3792854abc4fe658b3e38e81ecdb80f22456b53
-    return 0;
+  while (val <= 100) {
+    sum += val;
+    ++val;
+  }
+  std::cout << "Sum of 50 to 100 inclusive is " << sum << std::endl;
+  return 0;
 }
