e373cda2393f7a00a73773b944d34adf433ff2f1
Luka M <Luka M>
2017-02-21 09:35:07 +0800

    update

--- chapter02/Sales_data.h
+++ chapter02/Sales_data.h
@@ -4,9 +4,9 @@
 #include <string>
 
 struct Sales_data {
-    std::string bookNo;
-    unsigned units_sold = 0;
-    double revenue = 0.0;
+  std::string bookNo;
+  unsigned units_sold = 0;
+  double revenue = 0.0;
 }
 
 #endif
