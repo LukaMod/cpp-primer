ab8aa1572598f03f72cbce8818c694003bd380d9
Luka M <Luka M>
2017-02-21 11:08:38 +0800

    exercise3-43

---
+++ chapter03/3-43.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::begin;
+using std::end;
+
+int main()
+{
+    int ia[3][4] = {
+        {0, 1, 2, 3},
+        {4, 5, 6, 7},
+        {8, 9, 10, 11}};
+
+    for (const int(&row)[4] : ia)
+        for (int col : row)
+            cout << col << " ";
+    cout << endl;
+
+    for (size_t i = 0; i != 3; ++i)
+        for (size_t j = 0; j != 4; ++j)
+            cout << ia[i][j] << " ";
+    cout << endl;
+
+    for (int(*p)[4] = ia; p != ia + 3; ++p)
+        for (int *q = *p; q != *p + 4; ++q)
+            cout << *q << " ";
+    cout << endl;
+
+    return 0;
+}
