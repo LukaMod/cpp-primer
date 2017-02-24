8adee75fcfbba680b916abeb9bc4aba6045992b9
Luka M <Luka M>
2017-02-24 10:52:16 +0800

    exercise6-12.cpp

---
+++ chapter06/6-12.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+void swap(int &a, int &b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    int a, b;
+    cout << "please input two integers: ";
+    while (cin >> a >> b)
+    {
+        swap(a, b);
+        cout << a << " " << b << endl;
+        cout << "please input two integers: ";
+    }
+
+    return 0;
+}
