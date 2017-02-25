0fe5c2cc794a9919314004665c0ff53e95052e96
Luka <1004529488@qq.com>
2017-02-25 15:59:49 +0800

    exercise6-33

---
+++ chapter06/6-33.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using std::vector;
+using std::cout;
+using std::endl;
+
+void print(const vector<int> &vec, int index)
+{
+    if (index == vec.size())
+        return;
+    cout << vec[index++] << " ";
+    print(vec, index);
+}
+
+int main()
+{
+    vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    print(vec, 0);
+    cout << endl;
+    return 0;
+}
