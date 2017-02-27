c3446c6c33f344a56abe1d82a6ef8a3cde5c4a7a
Luka <1004529488@qq.com>
2017-02-27 20:54:39 +0800

    exercise 7.6

---
+++ chapter07/ex7_06.h
@@ -0,0 +1,48 @@
+#ifndef EX_7_06_H
+#define EX_7_06_H
+
+#include <string>
+using std::string;
+using std::istream;
+using std::ostream;
+
+struct Sales_data
+{
+    Sales_data &combine(const Sales_data &);
+    string isbn() const { return bookNo; }
+
+    string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+Sales_data &Sales_data::combine(const Sales_data &rhs)
+{
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs)
+{
+    Sales_data sum = lhs;
+    sum.combine(rhs);
+    return sum;
+}
+
+istream &read(istream &is, Sales_data &item)
+{
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = item.units_sold * price;
+    return is;
+}
+
+ostream &print(ostream &os, const Sales_data &item)
+{
+    os << item.isbn() << " " << item.units_sold << " "
+       << item.revenue;
+    return os;
+}
+
+#endif
