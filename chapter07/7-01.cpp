b89846682f4b13419d6bd9aa208e4789cf70cf46
Luka M <Luka M>
2017-02-27 17:11:38 +0800

    exercise7-01

--- chapter07/7-01.cpp
+++ chapter07/7-01.cpp
@@ -12,3 +12,35 @@ struct Sales_data
     unsigned units_sold = 0;
     double revenue = 0.0;
 };
+
+int main()
+{
+    Sales_data total;
+    if (cin >> total.bookNo >> total.units_sold >> total.revenue)
+    {
+        Sales_data trans;
+        while (cin >> trans.bookNo >> trans.units_sold >> trans.revenue)
+        {
+            if (total.bookNo == trans.bookNo)
+            {
+                total.units_sold += trans.units_sold;
+                total.revenue += trans.revenue;
+            }
+            else
+            {
+                cout << total.bookNo << " "
+                     << total.units_sold << " "
+                     << total.revenue << endl;
+                total = trans;
+            }
+        }
+        cout << total.bookNo << " "
+             << total.units_sold << " "
+             << total.revenue << endl;
+    }
+    else
+    {
+        cerr << "No data?!" << endl;
+    }
+    return 0;
+}
