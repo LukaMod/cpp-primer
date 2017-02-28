3205376216649e75d894f582dd0b6905e42e0bc8
Luka M <Luka M>
2017-02-28 12:34:14 +0800

    exercise 7.22

---
+++ chapter07/ex7_22.h
@@ -0,0 +1,36 @@
+#ifndef EX_7_15_H
+#define EX_7_15_H
+
+#include <string>
+
+class Person
+{
+    friend std::istream &read(std::istream &, Person &);
+
+  public:
+    Person() = default;
+    Person(const std::string &sname) : name(sname) {}
+    Person(const std::string &sname, const std::string &saddr) : name(sname), address(saddr) {}
+    Person(std::istream &is) { read(is, *this); }
+
+    const std::string &getName() const { return name; }
+    const std::string &getAddress() const { return address; }
+
+  private:
+    std::string name;
+    std::string address;
+};
+
+std::istream &read(std::istream &is, Person &item)
+{
+    is >> item.name >> item.address;
+    return is;
+}
+
+std::ostream &print(std::ostream &os, const Person &item)
+{
+    os << item.getName() << " " << item.getAddress();
+    return os;
+}
+
+#endif
