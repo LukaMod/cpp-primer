b8390665bf73dc393fa30a6f0facd0d24cd8c0ba
Luka M <Luka M>
2017-02-28 10:41:42 +0800

    update

--- chapter07/ex7_02.h
+++ chapter07/ex7_02.h
@@ -2,14 +2,13 @@
 #define EX_7_02_H
 
 #include <string>
-using std::string;
 
 struct Sales_data
 {
     Sales_data &combine(const Sales_data &);
-    string isbn() const { return bookNo; }
+    std::string isbn() const { return bookNo; }
 
-    string bookNo;
+    std::string bookNo;
     unsigned units_sold = 0;
     double revenue = 0.0;
 };
