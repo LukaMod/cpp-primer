74b5c5013fa1df262861ca7e433bd458b5651786
Luka M <Luka M>
2017-03-02 15:33:43 +0800

    exercise 7.53

---
+++ chapter07/ex7_53.h
@@ -0,0 +1,21 @@
+#ifndef EX_7_53_H_
+#define EX_7_53_H_
+
+class Debug
+{
+  public:
+    constexpr Debug(bool b = true) : hw(b), io(b), other(b) {}
+    constexpr Debug(bool h, bool i, bool o) : hw(h), io(i), other(o) {}
+
+    constexpr bool any() { return hw || io || other; }
+    void set_hw(bool b) { hw = b; }
+    void set_io(bool b) { io = b; }
+    void set_other(bool b) { other = b; }
+
+  private:
+    bool hw;
+    bool io;
+    bool other;
+};
+
+#endif
