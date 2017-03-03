e89cd3af1dfab6339fa22dc54a672233cb73661b
Luka M <Luka M>
2017-03-03 10:33:21 +0800

    exercise 8.6

---
+++ chapter08/ex8_06.cpp
@@ -0,0 +1,34 @@
+#include "../chapter07/ex7_41.h"
+#include <fstream>
+#include <iostream>
+
+using std::ifstream;
+using std::cout;
+using std::endl;
+using std::cerr;
+
+int main(int argc, char *argv[])
+{
+    ifstream input(argv[1]);
+    Sales_data total;
+    if (read(input, total))
+    {
+        Sales_data trans;
+        while (read(input, trans))
+        {
+            if (total.isbn() == trans.isbn())
+            {
+                total.combine(trans);
+            }
+            else
+            {
+                print(cout, total);
+                total = trans;
+            }
+        }
+        print(cout, total);
+    }
+    else
+        cerr << "No data?!" << endl;
+    return 0;
+}
