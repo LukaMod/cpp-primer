44db6980c3fe54349409b2d6770ab050c64b1cf4
Luka M <Luka M>
2017-03-13 13:42:06 +0800

    exercise 11.31

---
+++ chapter11/ex11_31.cpp
@@ -0,0 +1,29 @@
+#include <map>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    multimap<string, string> authors{
+        {"xyx", "flower"}, {"xyx", "tang"}, {"luka", "g"}, {"xyx", "sheep"}, {"uncle", "mine"}};
+    string author{"xyx"};
+    string work{"sheep"};
+
+    auto count = authors.count(author);
+    auto found = authors.find(author);
+    while (count)
+    {
+        if (found->second == work)
+        {
+            authors.erase(found);
+            break;
+        }
+        ++found;
+        --count;
+    }
+    for (const auto &au : authors)
+        cout << au.first << " " << au.second << endl;
+    return 0;
+}
