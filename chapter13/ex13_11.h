2f49c61c821283d51587ea5cd081ec86874255fd
Luka M <Luka M>
2017-03-17 16:00:31 +0800

    exercise 13.11

---
+++ chapter13/ex13_11.h
@@ -0,0 +1,24 @@
+#ifndef EX_13_11_H_
+#define EX_13_11_H_
+
+#include <string>
+
+class HasPtr
+{
+  public:
+    HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) {}
+    HasPtr(const HasPtr &hp) : ps(new std::string(*hp.ps)), i(hp.i) {}
+    HasPtr &operator=(const HasPtr &rhs)
+    {
+        delete ps;
+        ps = new std::string(*rhs.ps);
+        i = rhs.i;
+        return *this;
+    }
+    ~HasPtr() { delete ps; }
+  private:
+    std::string *ps;
+    int i;
+};
+
+#endif
