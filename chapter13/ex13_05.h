8b8c853f0b9341f003025ec747791ab1e6014a61
Luka M <Luka M>
2017-03-17 15:14:47 +0800

    exercise 13.5

---
+++ chapter13/ex13_05.h
@@ -0,0 +1,16 @@
+#ifndef EX_13_05_H_
+#define EX_13_05_H_
+
+#include <string>
+
+class HasPtr
+{
+  public:
+    HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) {}
+    HasPtr(const HasPtr &hp) : ps(new std::string(*hp.ps)), i(hp.i) {}
+  private:
+    std::string *ps;
+    int i;
+};
+
+#endif
