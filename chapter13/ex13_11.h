2cc4650104ceeec2df0b69c6f032964222e08d65
luka <1004529488@qq.com>
2017-03-19 20:33:40 +0800

    update

--- chapter13/ex13_11.h
+++ chapter13/ex13_11.h
@@ -5,20 +5,21 @@
 
 class HasPtr
 {
-  public:
-    HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) {}
-    HasPtr(const HasPtr &hp) : ps(new std::string(*hp.ps)), i(hp.i) {}
-    HasPtr &operator=(const HasPtr &rhs)
-    {
-        delete ps;
-        ps = new std::string(*rhs.ps);
-        i = rhs.i;
-        return *this;
-    }
-    ~HasPtr() { delete ps; }
-  private:
-    std::string *ps;
-    int i;
+public:
+  HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) {}
+  HasPtr(const HasPtr &hp) : ps(new std::string(*hp.ps)), i(hp.i) {}
+  HasPtr &operator=(const HasPtr &rhs)
+  {
+    auto newp = new std::string(*rhs.ps);
+    delete ps;
+    ps = newp;
+    i = rhs.i;
+    return *this;
+  }
+  ~HasPtr() { delete ps; }
+private:
+  std::string *ps;
+  int i;
 };
 
 #endif
