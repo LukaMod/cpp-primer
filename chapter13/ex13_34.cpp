28bf8ffe635881dbe9b0cff015823ceebafc79f2
Luka M <Luka M>
2017-03-20 14:20:55 +0800

    exercise 13.34

---
+++ chapter13/ex13_34.cpp
@@ -0,0 +1,59 @@
+#include "ex13_34.h"
+
+#include <iostream>
+
+#include "ex13_36.h"
+
+Message::Message(const Message &m) : contents(m.contents), folders(m.folders)
+{
+    add_to_Folders(m);
+}
+
+Message &Message::operator=(const Message &rhs)
+{
+    remove_from_Folders();
+    contents = rhs.contents;
+    folders = rhs.folders;
+    add_to_Folders(rhs);
+    return *this;
+}
+
+Message::~Message()
+{
+    remove_from_Folders();
+}
+
+void Message::save(Folder &f)
+{
+    folders.insert(&f);
+    f.addMsg(this);
+}
+
+void Message::remove(Folder &f)
+{
+    folders.erase(&f);
+    f.remMsg(this);
+}
+
+void Message::add_to_Folders(const Message &)
+{
+    for (auto &f : folders)
+        f->addMsg(this);
+}
+
+void Message::remove_from_Folders()
+{
+    for (auto &f : folders)
+        f->remMsg(this);
+}
+
+void swap(Message &lhs, Message &rhs)
+{
+    using std::swap;
+    lhs.remove_from_Folders();
+    rhs.remove_from_Folders();
+    swap(lhs.contents, rhs.contents);
+    swap(lhs.folders, rhs.folders);
+    lhs.add_to_Folders(lhs);
+    rhs.add_to_Folders(rhs);
+}
