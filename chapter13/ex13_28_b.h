6e7335bcf3e4d1a751a30bd3bcdcd70f814e0363
Luka M <Luka M>
2017-03-20 14:21:35 +0800

    update

--- chapter13/ex13_28_b.h
+++ chapter13/ex13_28_b.h
@@ -1,23 +1,23 @@
 #ifndef EX_13_28_B_H_
 #define EX_13_28_B_H_
 
-#include "TreeNode.h"
+#include "ex13_28_a.h"
 
 class BinStrTree
 {
-  public:
-    BinStrTree() : root(new TreeNode()) {}
-    BinStrTree(const BinStrTree &rhs) : root(new TreeNode(*rhs.root)) {}
-    BinStrTree &operator=(const BinStrTree &rhs)
-    {
-        auto newroot = new TreeNode(*rhs.root);
-        this->~BinStrTree();
-        root = newroot;
-        return *this;
-    }
-    ~BinStrTree() { delete root; }
-  private:
-    TreeNode *root;
+public:
+  BinStrTree() : root(new TreeNode()) {}
+  BinStrTree(const BinStrTree &rhs) : root(new TreeNode(*rhs.root)) {}
+  BinStrTree &operator=(const BinStrTree &rhs)
+  {
+    auto newroot = new TreeNode(*rhs.root);
+    this->~BinStrTree();
+    root = newroot;
+    return *this;
+  }
+  ~BinStrTree() { delete root; }
+private:
+  TreeNode *root;
 };
 
 #endif
