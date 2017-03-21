d0bfeb67dc4a01af8e72bf07f820d93bb930bc81
Luka M <Luka M>
2017-03-21 15:24:55 +0800

    exercise 14.02

---
+++ chapter14/ex14_02.cpp
@@ -0,0 +1,30 @@
+#include "ex14_02.h"
+
+Sales_data &Sales_data::operator+=(const Sales_data &rhs)
+{
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+Sales_data operator+(const Sales_data &lhs, const Sales_data &rhs)
+{
+    Sales_data sum = lhs;
+    sum += rhs;
+    return sum;
+}
+
+std::istream &operator>>(std::istream &is, Sales_data &item)
+{
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = item.units_sold * price;
+    return is;
+}
+
+std::ostream &operator<<(std::ostream &os, const Sales_data &item)
+{
+    os << item.isbn() << " " << item.units_sold << " "
+       << item.revenue;
+    return os;
+}
---
+++ chapter14/ex14_02.h
@@ -0,0 +1,31 @@
+#ifndef EX_14_02_H_
+#define EX_14_02_H_
+
+#include <string>
+
+class Sales_data
+{
+    friend Sales_data operator+(const Sales_data &, const Sales_data &);
+    friend std::istream &operator>>(std::istream &, Sales_data &);
+    friend std::ostream &operator<<(std::ostream &, const Sales_data &);
+
+  public:
+    Sales_data(const std::string &s, unsigned n, double p) : bookNo(s), units_sold(n), revenue(n * p) {}
+    Sales_data() : Sales_data("", 0, 0.0) {}
+    Sales_data(const std::string &s) : Sales_data(s, 0, 0.0) {}
+    Sales_data(std::istream &is) : Sales_data()
+    {
+        is >> *this;
+    }
+
+    Sales_data &operator+=(const Sales_data &);
+    std::string isbn() const { return bookNo; }
+
+  private:
+    double avg_price() const { return units_sold ? revenue / units_sold : 0; }
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+#endif
