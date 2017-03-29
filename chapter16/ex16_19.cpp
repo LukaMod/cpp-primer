6c49101a996e2d23dd113cd4814704d2aed8bbc7
Luka M <Luka M>
2017-03-29 14:47:44 +0800

    exercise 16.19

---
+++ chapter16/ex16_19.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+template <typename C>
+void print(const C &c)
+{
+    typedef typename C::size_type size_type;
+    auto iter = c.begin();
+    for (size_type i = 0; i != c.size(); ++i)
+        cout << *iter++ << " ";
+}
+
+int main()
+{
+    vector<int> vec{0, 1, 2, 3, 4, 5, 6, 7};
+    print(vec);
+    cout << endl;
+    unordered_set<int> uset{1, 2, 3, 4, 5, 6, 7};
+    print(uset);
+    cout << endl;
+    return 0;
+}
