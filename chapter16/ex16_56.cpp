1364a3a2ab665b572b511475ef7ebd5443ee74f7
Luka M <Luka M>
2017-04-06 09:47:39 +0800

    exercise 16.56

---
+++ chapter16/ex16_56.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+template <typename T>
+string debug_rep(const T &t)
+{
+    ostringstream ret;
+    ret << t;
+    return ret.str();
+}
+
+template <typename T>
+string debug_rep(T *p)
+{
+    ostringstream ret;
+    ret << "pointer: " << p;
+    if (p)
+        ret << " " << debug_rep(*p);
+    else
+        ret << " null pointer";
+    return ret.str();
+}
+
+string debug_rep(const string &s)
+{
+    return '"' + s + '"';
+}
+
+string debug_rep(char *p)
+{
+    return debug_rep(string(p));
+}
+
+string debug_rep(const char *p)
+{
+    return debug_rep(string(p));
+}
+
+template <typename T>
+ostream &print(ostream &os, const T &t)
+{
+    return os << t;
+}
+
+template <typename T, typename... Args>
+ostream &print(ostream &os, const T &t, const Args &... rest)
+{
+    os << t << ",";
+    return print(os, rest...);
+}
+
+template <typename... Args>
+ostream &errorMsg(ostream &os, const Args &... rest)
+{
+    return print(os, debug_rep(rest)...);
+}
+
+int main()
+{
+    errorMsg(cout, 1993, 7, 14, "Y", "M", "D");
+    return 0;
+}
