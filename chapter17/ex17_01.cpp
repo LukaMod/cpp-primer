e4525beafb872212ae5b2ab3472174807f11ca8b
Luka M <Luka M>
2017-04-07 11:01:48 +0800

    exercise 17.1

---
+++ chapter17/ex17_01.cpp
@@ -0,0 +1,13 @@
+#include <tuple>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    tuple<int, int, int> t1{10, 20, 30};
+    tuple<string, vector<string>, pair<string, int>> t2;
+
+    return 0;
+}
