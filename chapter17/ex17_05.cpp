8152de1ad19a49e6679ab3c885c4a07703bc1108
Luka M <Luka M>
2017-04-10 10:54:02 +0800

    exercise 17.5

---
+++ chapter17/ex17_05.cpp
@@ -0,0 +1,37 @@
+#include "action.h"
+
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+vector<matches_pair> findBook(const vector<vector<Sales_data>> &files, const string &book)
+{
+    vector<matches_pair> ret;
+    for (auto it = files.cbegin(); it != files.cend(); ++it)
+    {
+        auto found = equal_range(it->cbegin(), it->cend(), book,
+                                 [](const Sales_data &lhs, const Sales_data &rhs) { return lhs.isbn() < rhs.isbn(); });
+        if (found.first != found.second)
+            ret.push_back(make_pair(it - files.cbegin(), make_pair(found.first, found.second)));
+    }
+    return ret;
+}
+
+void reportResults(istream &in, ostream &os, const vector<vector<Sales_data>> &files)
+{
+    string s;
+    while (in >> s)
+    {
+        auto trans = findBook(files, s);
+        if (trans.empty())
+        {
+            cout << s << " not found in any stores" << endl;
+            continue;
+        }
+        for (const auto &store : trans)
+            os << "store " << store.first << " sales: "
+               << accumulate(store.second.first, store.second.second, Sales_data(s))
+               << endl;
+    }
+}
---
+++ chapter17/ex17_05.h
@@ -0,0 +1,12 @@
+#include <vector>
+
+#include "ex14_02.h"
+
+typedef std::pair<std::vector<Sales_data>::size_type,
+                  std::pair<std::vector<Sales_data>::const_iterator,
+                            std::vector<Sales_data>::const_iterator>>
+    matches_pair;
+
+std::vector<matches_pair> findBook(const std::vector<std::vector<Sales_data>> &, const std::string &);
+
+void reportResults(std::istream &, std::ostream &, const std::vector<std::vector<Sales_data>> &);
