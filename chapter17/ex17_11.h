a8f0774e3a04fa64a293ebcc4ef83ba04829a2cf
Luka M <Luka M>
2017-04-13 17:14:04 +0800

    exercise 17.11

---
+++ chapter17/ex17_11.h
@@ -0,0 +1,35 @@
+#ifndef QUIZ_H_
+#define QUIZ_H_
+
+#include <bitset>
+#include <string>
+#include <iostream>
+//
+//template<unsigned> class quiz;
+//template<unsigned N>
+//std::ostream& operator<<(std::ostream&, const quiz<N>&);
+
+template <unsigned N>
+class quiz
+{
+  public:
+    quiz() = default;
+    quiz(std::string &s) : b(s) {}
+
+    void update(unsigned i, bool ans) { b.set(i, ans); }
+
+  private:
+    template <unsigned M>
+    friend std::ostream &operator<<(std::ostream &os, const quiz<M> &q);
+
+    std::bitset<N> b;
+};
+
+template <unsigned M>
+std::ostream &operator<<(std::ostream &os, const quiz<M> &q)
+{
+    os << q.b;
+    return os;
+}
+
+#endif // !QUIZ_H_
